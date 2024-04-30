dd80c6ed3bf8c8aa794a9b7ce487d89d62c517cd
陈劲全 <chenjingquan@dabby.com.cn>
2024-04-30 14:01:19 +0800

    新增工具类

---
+++ src/utils/desensitize.ts
@@ -0,0 +1,32 @@
+const desensitizeUtil = { desensitizeValue };
+export default desensitizeUtil;
+
+export function desensitizeValue(
+  input: string,
+  maskIndexes?: number[]
+): string {
+  if (!input) return "";
+  const length = input.length;
+  // 如果未提供脱敏下标数组，则默认对整个输入值进行全脱敏
+  if (!maskIndexes || maskIndexes.length === 0) {
+    return "*".repeat(length);
+  }
+
+  // 创建一个标记数组，用于标记需要脱敏的位置
+  const shouldMask = new Array(length).fill(false);
+
+  // 将指定的脱敏下标位置设为 true
+  maskIndexes.forEach((index) => {
+    if (index >= 0 && index < length) {
+      shouldMask[index] = true;
+    }
+  });
+
+  // 生成脱敏后的值
+  let desensitizedValue = "";
+  for (let i = 0; i < length; i++) {
+    desensitizedValue += shouldMask[i] ? "*" : input[i];
+  }
+
+  return desensitizedValue;
+}
---
+++ src/utils/requests.ts
@@ -0,0 +1,158 @@
+import router from "../router";
+import axios from "axios";
+import { showDialog } from "vant/lib/dialog";
+import { showNotify } from "vant/lib/notify";
+
+// axios.defaults.withCredentials = true;
+// const BASE_URL = window.location.origin + '/dbvisitorsvc'
+
+const { VITE_AXIOS_BASEURL, VITE_API_ENV } = import.meta.env;
+//创建axios的一个实例
+let instance = axios.create({
+  baseURL: VITE_API_ENV === "dev" ? "/api" : window.location.origin,
+  timeout: 60000, //设置超时
+});
+
+/**
+ * 请求拦截器
+ */
+instance.interceptors.request.use(
+  (config) => {
+    config.headers = {
+      "Content-Type": "application/json",
+      ...config.headers,
+    };
+    let token = localStorage.getItem("token") || "";
+    if (token) {
+      config.headers["token"] = JSON.parse(token);
+    }
+    // if (token) config.data.token = token;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+/**
+ * 响应拦截器
+ */
+instance.interceptors.response.use(
+  (response) => {
+    // console.log({ response });
+
+    if ([2001, 4008, 4020, 4013, 4014].indexOf(response.data.retCode) != -1) {
+      sessionStorage.clear();
+      router.replace({
+        path: "/login",
+        query: {
+          qywxType: 4,
+        },
+      });
+      return response.data;
+    }
+    return response.data;
+  },
+  (error) => {
+    console.log({ error });
+    return Promise.reject(error);
+  }
+);
+
+interface RequestMsg {
+  errorMsg?: boolean;
+  errorText?: string;
+  successMsg?: boolean;
+  successText?: string;
+}
+
+interface RequestConfig {
+  msgConfig?: RequestMsg;
+  showMsg?: boolean;
+  headers?: Object;
+  requestConfig?: Object;
+}
+
+const _request = async (
+  method: string,
+  url: string,
+  params: object,
+  config: RequestConfig = {
+    msgConfig: {
+      errorMsg: true,
+      errorText: "",
+      successMsg: false,
+      successText: "",
+    },
+    showMsg: false,
+    headers: {},
+    requestConfig: {},
+  }
+): Promise<any> => {
+  const { errorMsg, successMsg, successText = "" } = config?.msgConfig || {};
+  const { showMsg } = config || {};
+  const { headers = {}, requestConfig = {} } = config;
+  try {
+    let res = null;
+    // 处理不同请求的入参形式
+    switch (method) {
+      case "post":
+        res = await instance.post(url, params, {
+          headers,
+          ...requestConfig,
+        });
+        break;
+      case "get":
+        res = await instance.get(url, { params, headers });
+        break;
+    }
+
+    const { retCode: retCode, retMessage: retMessage } = res as any;
+    // 错误提示处理，这里只处理错误提示，逻辑还是需要在外层判断retCode
+    if (retCode !== 0 && (showMsg || errorMsg)) {
+      showDialog({
+        title: "提示",
+        message: retMessage,
+      });
+
+      return Promise.reject(res)
+    }
+    // 成功提示处理
+    if (retCode == 0 && (showMsg || successMsg || successText)) {
+      showNotify({ type: "success", message: successText || "操作成功" });
+    }
+
+    return res;
+  } catch (err) {
+    if (showMsg || errorMsg) {
+      showDialog({
+        title: "网络错误",
+        message: JSON.stringify(err),
+      });
+    }
+  }
+};
+
+export const post = async <T>(
+  url: string,
+  params: object,
+  config: RequestConfig
+): Promise<T> => {
+  console.log(url + "-param=====", params);
+  let data = await _request("post", url, params, config);
+  console.log(url + "-resp=====", data);
+  return data;
+};
+
+export const get = async <T>(
+  url: string,
+  params: object,
+  config: RequestConfig
+): Promise<T> => {
+  console.log(url + "-param=====", params);
+
+  const data = await _request("get", url, params, config);
+  console.log(url + "-resp=====", data);
+
+  return data;
+};
